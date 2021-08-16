ce522c83167ca9d0518c54c20f190a15f6df0451
ISAAC IGE <isaacige@ISAACs-MacBook-Pro.local>
2021-08-16 16:43:04 +0100

    Added views and route controller

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use(adminRoutes)
+app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,14 @@ const express = require('express')
 
 const router = express.Router()
 
-//another route
-router.get('/addProduct', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Submit </button></form>')
+// admin get request for add product
+router.get('/add-product', (req, res, next) => {
+    res.send(
+        '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit"> Submit </button></form>')
 })
 
-// create product submission endpoint
-router.post('/product', (req, res, next) => {
+// admin post request for add product
+router.post('/add-product', (req, res, next) => {
     console.log(req.body)
     res.redirect('/')
 })
